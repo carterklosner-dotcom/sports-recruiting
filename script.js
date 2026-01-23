fb5e76a99774bc7e79ba3ff1084854bb7041656c
carterklosner-dotcom <carter.klosner@gmail.com>
2026-01-23 12:13:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,6 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 function selectRole(role) {
   localStorage.setItem("role", role);
   window.location.href = "sport.html";
